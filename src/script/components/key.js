41e60974fc565a95c5727385af430bb167f8d516
Illia Sakharau <sakharovia0204@gmail.com>
2023-04-30 15:00:08 +0400

    feat: add action for Backspace, Delete, Capslock, Shift keys

--- src/script/components/key.js
+++ src/script/components/key.js
@@ -45,21 +45,3 @@ export default class Key {
     this.key.textContent = keyText;
   }
 }
-
-// export default class Key {
-//   constructor(keyCode, char) {
-//     this.keyCode = keyCode;
-//     this.key = document.createElement('div');
-//     this.key.setAttribute('data-code', keyCode);
-//     this.key.classList.add('key-board__key', 'key');
-//     this.setChar(char);
-//   }
-
-//   getKey() {
-//     return this.key;
-//   }
-
-//   setChar(char) {
-//     this.key.textContent = char;
-//   }
-// }
--- src/script/index.js
+++ src/script/index.js
@@ -33,25 +33,73 @@ main.appendChild(textAreaSection);
 main.appendChild(CreateKeyboard(state, clickDownKey, clickUpKey));
 
 
-
-
-function clickDownKey() {
-  this.classList.add('key_pressed');
-  
+function modificateText(step, char) {
   const startPos = textArea.selectionStart;
   const endPos = textArea.selectionEnd;
   const scrollTop = textArea.scrollTop;
 
-  textArea.value = textArea.value.substring(0, startPos) + KEYS[this.dataset.code][state] + textArea.value.substring(endPos, textArea.value.length);
+  if (char === 'Backspace') {
+    textArea.value = textArea.value.substring(0, startPos + step) + textArea.value.substring(endPos, textArea.value.length);
+  } else if (char === 'Delete') {
+    textArea.value = textArea.value.substring(0, startPos) + textArea.value.substring(endPos + 1, textArea.value.length);
+  } else {
+    textArea.value = textArea.value.substring(0, startPos) + char + textArea.value.substring(endPos, textArea.value.length);
+  }
+  
 
-  textArea.selectionStart = startPos + 1;
-  textArea.selectionEnd = startPos + 1;
+  textArea.selectionStart = startPos + step;
+  textArea.selectionEnd = startPos + step;
+  textArea.scrollTop = scrollTop;
+}
 
+function reverseCase(key) {
+
+
+  if(key.dataset.code === 'CapsLock') {
+    key.classList.toggle('key_active');
+  };
+  
+  if (state % 2 === 0) {
+    state++;
+  } else {
+    state--;
+  }
+
+  for (let key in KEYS) {
+    document.querySelector(`.key[data-code="${key}"]`).textContent = KEYS[key][4] || KEYS[key][state];
+  }
+}
+
+
+function clickDownKey() {
   event.preventDefault();
+
+  this.classList.add('key_pressed');
+
+  if (KEYS[this.dataset.code][state]) {   //если нажат символ
+    modificateText(1, KEYS[this.dataset.code][state]);
+  }
+  if (this.dataset.code === 'Backspace') {    //если нажат Backspace
+    modificateText(-1, 'Backspace');
+  }
+  if (this.dataset.code === 'Delete') {    //если нажат Delete
+    modificateText(0, 'Delete');
+  }
+  if (this.dataset.code === 'CapsLock' && !event.repeat) {    //если нажат CapsLock
+    reverseCase(this);
+  }
+  if ((this.dataset.code === 'ShiftLeft' || this.dataset.code === 'ShiftRight') && !event.repeat) {    //если нажат Shift
+    reverseCase(this);
+  }
+
 }
 
 function clickUpKey() {
 this.classList.remove('key_pressed');
+
+if (this.dataset.code === 'ShiftLeft' || this.dataset.code === 'ShiftRight') {    //если отпущен Shift
+  reverseCase(this);
+}
 
 return false;
 }
